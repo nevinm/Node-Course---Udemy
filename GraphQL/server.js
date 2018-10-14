729ee4df127ba9faa7da4e3f4841f4bbb3b36d15
Nevin Madhukar K <nevin.madhukar.k@gmail.com>
2018-10-14 23:28:32 +0530

    Graph QL Stuff

---
+++ GraphQL/server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const expressGraphQL = require('express-graphql');
+const schema = require('./schema/schema');
+
+const app = express();
+
+// Development server property.
+app.use('/graphql', expressGraphQL({
+    schema,
+    graphiql: true
+}));
+
+app.listen(4000, () => {
+    console.log('Server started at 4000');
+});
